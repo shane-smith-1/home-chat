5f06c0dc65ebd4914ee30f99eb965f1425daf097
shane-cw <shane@getclockwise.com>
2023-05-30 15:34:53 -0700

    instructions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,22 @@ import ClientSection from "./ClientSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-4 text-center">
-      <h1 className="my-4 text-5xl font-bold">Home Repair Quote Tool</h1>
+    <main className="mb-5 flex min-h-screen flex-col items-center justify-center gap-4 text-center">
+      <div className="mx-3">
+        <h1 className="my-4 text-center text-5xl font-bold">
+          Home Repair Quote AI
+        </h1>
+        <p className="my-8 max-w-xl">
+          1. Paste in your quote. No formatting needed.
+          <br />
+          2. Ask any of our preset questions about your quote, or ask your own.
+          <br />
+          3. Sign-up for notifications about regional, state, and federal tax
+          incentives you may qualify for.
+          <br />
+          4. Use this site to submit questions to your contractor
+        </p>
+      </div>
       <div className="flex w-full flex-col items-center gap-2 font-mono md:flex-row">
         <ClientSection />
       </div>
